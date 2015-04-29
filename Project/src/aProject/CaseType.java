2c68868c230aeee14cbfc9140aa1d818b28dd61a
rouge1 <sirinsc@yahoo.com>
2015-04-29 13:26:54 -0700

    More comments. I hope this works

--- Project/src/aProject/CaseType.java
+++ Project/src/aProject/CaseType.java
@@ -1,53 +1,33 @@
 package aProject;
 
 /**
-<<<<<<< HEAD
- * This class is enum for CaseType 
-=======
  * This enum lists all of the possible (accident) case types.
  * The case type is set for each accident (data point).
  * @author Group #2
  * @version 1.0
->>>>>>> refs/remotes/origin/master
  */
 
 public enum CaseType {
 	BICYCLE,PEDESTRIAN;
-<<<<<<< HEAD
+
 
 	/**
 	 * This method sets the caseType based on 
 	 *  
-	 * @param String to set the CaseType to
-	 * @return the CaseType equal to the String version of the caseType
-	 */
-	static public CaseType setType(String input){
-=======
-	
-	/**
-	 * The setType method sets the case type.
 	 * @param s String. The accident's case type.
 	 * @return The accident's (data point's) case type.
 	 */
-	
-	static public CaseType setType(String s){
-		
->>>>>>> refs/remotes/origin/master
+	static public CaseType setType(String input){
+
 		CaseType temp = null;
-		
+
 		try{
-<<<<<<< HEAD
 			temp = CaseType.valueOf(input);
 		}catch(IllegalArgumentException iae){
-=======
-			temp = CaseType.valueOf(s);
-		} 
-		catch(IllegalArgumentException iae){
->>>>>>> refs/remotes/origin/master
 			System.out.println("Error: " + iae.toString());
 		} // end of try/catch
-		
+
 		return temp;
 	} // end of setType method
-	
+
 } // end of enum CaseType
--- Project/src/aProject/DataPoint.java
+++ Project/src/aProject/DataPoint.java
@@ -1,91 +1,115 @@
-
 package aProject;
+
+/**
+ * The DataPoint class stores data about an accident (case)
+ * for Group #2's term group programming project.
+ * @author Group #2
+ * @version 1.0
+ */
 
 import java.util.Date;
 
 public class DataPoint extends Point{
-	private int caseNumber;
-	private CaseType theCaseType;
-	private Date theDate;
+	private int caseNumber;			// Variable to store accident's case number.
+	private CaseType theCaseType;	// Variable to store the accident's case type.
+	private Date theDate;			// Variable to store the date of the accident.
 
 	/**
-	 * @param caseNumber
-	 * @param type
-	 * @param theDate
-	 * @param latitude
-	 * @param longitude
+	 * This constructor initializes the data point's case number, case type, and case date.
+	 * It also initializes the data point's latitude and longitude to 0 using the
+	 * Point (parent class) class's default constructor.
+	 * @param caseNumber int. The data point's case number.
+	 * @param type CaseType. The data point's case type.
+	 * @param theDate Date. The data point's case date.
+	 * @param latitude double. The data point's longitude.
+	 * @param longitude double. The data point's latitude.
 	 */
 	public DataPoint(int caseNumber, CaseType type, Date theDate, double latitude, double longitude) {
-		super(latitude,longitude,0);
+		super(latitude,longitude,0); 
 		this.caseNumber = caseNumber;
 		this.theCaseType = type;
 		this.theDate = theDate;
-	}//end constructor
+	} // end of constructor
 
 	/**
-	 * @param caseNumber
-	 * @param type
-	 * @param theDate
-	 * @param latitude
-	 * @param longitude
-	 * @param elevation 
+	 * This constructor initializes the data point's case number, case type, and case date.
+	 * It also initializes the data point's latitude, longitude, and elevation using the
+	 * Point (parent class) class's default constructor.
+	 * @param caseNumber int. The data point's case number.
+	 * @param type CaseType. The data point's case type.
+	 * @param theDate Date. The data point's case date.
+	 * @param latitude double. The data point's latitude.
+	 * @param longitude double. The data point's longitude.
+	 * @param elevation double. The data point's elevation.
 	 */
 	public DataPoint(int caseNumber, CaseType type, Date theDate, double latitude, double longitude, double elevation) {
 		super(latitude,longitude,elevation);
 		this.caseNumber = caseNumber;
 		this.theCaseType = type;
 		this.theDate = theDate;
-	}//end constructor
+	} // end of constructor
 	
 	/**
-	 * @return the caseNumber
+	 * The getCaseNumber method returns the data point's
+	 * case number.
+	 * @return The data point's case number.
 	 */
 	public int getCaseNumber() {
 		return caseNumber;
-	}
+	} // end of getCaseNumber method
 	
 	/**
-	 * @param caseNumber the caseNumber to set
+	 * The setCaseNumber method sets the data point's
+	 * case number.
+	 * @param caseNumber int. The data point's case number.
 	 */
 	public void setCaseNumber(int caseNumber) {
 		this.caseNumber = caseNumber;
-	}
+	} // end of setCaseNumber method
 	
 	/**
-	 * @return the caseType
+	 * The getCaseType method returns the data point's
+	 * case type.
+	 * @return The data point's case type.
 	 */
 	public CaseType getCaseType() {
 		return theCaseType;
-	}
+	} // end of getCaseType method
 	
 	/**
-	 * @param caseType the caseType to set
+	 * The setCaseType method sets the data point's
+	 * case type.
+	 * @param caseType CaseType. The data point's case type.
 	 */
 	public void setCaseType(CaseType caseType) {
 		this.theCaseType = caseType;
-	}
+	} // end of setCaseType method
 	
 	/**
-	 * @return the theDate
+	 * The getTheDate method returns the data point's 
+	 * case date (accident date).
+	 * @return The data point's case date.
 	 */
 	public Date getTheDate() {
 		return theDate;
-	}
+	} // end of getTheDate method
 	
 	/**
-	 * @param theDate the theDate to set
+	 * The setTheDate method sets the data point's case date.
+	 * @param theDate Date. The data point's case date.
 	 */
 	public void setTheDate(Date theDate) {
 		this.theDate = theDate;
-	} 
+	} // end of setTheDate method
 	
 	/**
-	 * @Override theDate the theDate to set
+	 * The toString method sets the data point's attributes to String
+	 * and overrides the superclass's (Point class) method.
+	 * @Override
 	 */
 	public String toString() {
 		String temp = String.format("%10s %10s %10s %23s", caseNumber, theCaseType, theDate, this.getPosition().toString());
 		return temp;
-	} 
-	
-	
-}//end class DataPoint
+	} // end of toString method
+		
+} // end of class DataPoint
